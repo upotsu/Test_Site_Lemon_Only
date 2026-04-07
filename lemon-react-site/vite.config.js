4b7a630a1ba78e2def117875248fc12c7f0b1339
upotsu <apeiron365@outlook.jp>
2026-04-07 21:35:20 +0900

    Update vite.config.js

--- lemon-react-site/vite.config.js
+++ lemon-react-site/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Test_Site_Lemon_Only/'
+  base: '/Test_Site_Lemon_Only/github/workflows/deploy.yml'
 })
