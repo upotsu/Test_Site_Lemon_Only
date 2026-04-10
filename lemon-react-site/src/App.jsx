c4aca60e8547e2681300637b84dab9681fa34f28
saitoh_t <apeiron365@outlook.jp>
2026-04-10 19:25:45 +0900

    Fix React entry files for GitHub Pages

--- lemon-react-site/src/App.jsx
+++ lemon-react-site/src/App.jsx
@@ -1,16 +1,8 @@
-import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Home from './pages/Home'
-import Experience from './pages/Experience'
-
 export default function App() {
   return (
-    <div className="app-shell">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/experience" element={<Experience />} />
-      </Routes>
-    </div>
+    <main>
+      <h1>LEMON SHOCK</h1>
+      <p>Reactサイトの表示確認中です。</p>
+    </main>
   )
 }
